325ea4ac99b2fa5e6a933206bb6d080c2428393e
Jenkkss <56266569+Jenkkss@users.noreply.github.com>
2021-03-02 14:50:52 +0000

    Update database.js

--- client/src/firebase/database.js
+++ client/src/firebase/database.js
@@ -2,15 +2,15 @@ import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
 
-const firebaseConfig = {
-    apiKey: "AIzaSyC5jT1LPf8MUa4BaoAMTg7kdSm0WJzHauM",
-    authDomain: "cardiomyopathy-project-94ccf.firebaseapp.com",
-    projectId: "cardiomyopathy-project-94ccf",
-    storageBucket: "cardiomyopathy-project-94ccf.appspot.com",
-    messagingSenderId: "673422606828",
-    appId: "1:673422606828:web:71b24533166e9d155100c9"
-};
 
+const firebaseConfig = {
+    apiKey: "AIzaSyCzk50z63UkWyUnkZFnEw4XmPWrWMurxuQ",
+    authDomain: "cardiomyopathy-ba686.firebaseapp.com",
+    projectId: "cardiomyopathy-ba686",
+    storageBucket: "cardiomyopathy-ba686.appspot.com",
+    messagingSenderId: "287154487963",
+    appId: "1:287154487963:web:1ae5b7b5a7aecd74cdfc3e"
+  };
 firebase.initializeApp(firebaseConfig);
 const firebaseAuthentication = firebase.auth();
 const firebaseFireStore = firebase.firestore();
